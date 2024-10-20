bf08ac591d1f072c764f66af07452bcf1ad24a6e
nayanprasad <nayanprasad096@gmail.com>
2024-10-21 04:01:00 +0530

    init

---
+++ server/dist/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createClient } from "redis";
+const app = express();
+app.use(express.json());
+const client = await createClient()
+    .on('error', err => console.log('Redis Client Error', err))
+    .connect();
+app.post("/submit", async (req, res) => {
+    try {
+        const { id } = req.body;
+        await client.lPush("submission", JSON.stringify(id));
+        res.status(200).json({
+            success: true
+        });
+    }
+    catch (err) {
+        res.status(500).json({
+            success: false
+        });
+    }
+});
+app.listen(6000);
---
+++ worker/src/index.ts
@@ -0,0 +1,16 @@
+import {createClient} from 'redis';
+
+const client = createClient()
+
+
+async function main() {
+    await client.connect()
+    while (true) {
+        const data = await client.BRPOP("submission", 0)
+        console.log(data)
+        await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+}
+
+main()
+
